33ad419e6d3e97ddc9082fe3e13138f723b2fcb6
SanduPalii <palii.sandu@gmail.com>
2022-02-14 21:05:23 +0200

    update

--- groupTask/groupTask_team12.php
+++ groupTask/groupTask_team12.php
@@ -7,15 +7,41 @@
 
 # 1. Create/read a text file by using approprite php functions 
     # Step 1: check if file exists or not
-    # Step 2: Open the file using appropriate mode. (each member opens the file in different mode)
-    # Step 3: Use fwrite/fread function to write/read on the file your team name and members name. 
-    # Step 4: Close the file 
+date_default_timezone_set('Europe/Helsinki');
+echo "<h2>Checking if file exists or not</h2>";
+if (file_exists("groupTask/new_file.txt")){
+    echo "file exisits";
+}
+else 
+    echo "file not found";
+    echo "<hr><h2>Creating a file </h2>";
+# Step 2: Open the file using appropriate mode. (each member opens the file in different mode)
+$newfile = fopen('new_file.txt','w') or die ("Failed to create a file");
+# Step 3: Use fwrite/fread function to write/read on the file your team name and members name. 
+$txt = "Hello world this is interesting. <br>";
+    fwrite($newfile, $txt);  
+# Step 4: Close the file 
+fclose($newfile);
+?>
 
+
+<?
 #2. Uploaing files 
-     # Step 1: Create a simple html form to upload a file. 
-     # Step 2: You are required to limit the upload file size to 2 MB. 
-     # Step 3: Make sure that users can submit only images. 
-     # Step 4: Upon successful upload, you print a message "File uploaded successfully" and also 
-     # provide the link to the directory where files are uploaded.
+# Step 1: Create a simple html form to upload a file. 
+?>
+<hr><h2>Upload your image! Max 2MB </h2>
 
+<form enctype="multipart/form-data" action="upload.php" method="POST">
+    Select file to upload: <input name="userfile" type="file" required><br>
+    <input type="submit" value="Upload File" />
+</form>
+<hr><h2>PHP Date function </h2>
+<?
+# Step 2: You are required to limit the upload file size to 2 MB.
+# --- The MAX_FILE_SIZE item cannot specify a file size greater than the file size that has been set in the upload_max_filesize in the php.ini file. The default is 2 megabytes.
+
+# Step 3: Make sure that users can submit only images. 
+
+# Step 4: Upon successful upload, you print a message "File uploaded successfully" and also 
+# provide the link to the directory where files are uploaded.
 ?>
---
+++ groupTask/upload.php
@@ -0,0 +1,11 @@
+<?php
+date_default_timezone_set('Europe/Helsinki');
+//target_dir is the location where you want your files to be uploaded
+
+$dest = "images/".basename($_FILES["userfile"] ["name"]);
+    if (move_uploaded_file($_FILES["userfile"] ["tmp_name"], $dest)){
+        echo "The file has been successfully uploaded. <br>";
+        echo "<a href='/images/'> Click to see list of files </a>";
+    }
+    else "Unable to upload file";
+?>
