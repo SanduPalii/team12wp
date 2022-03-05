03f30a93e69babf584ed0dd23aafbf21eb4f56e0
SanduPalii <palii.sandu@gmail.com>
2022-03-05 23:13:07 +0200

    All project

---
+++ coliving/main/apartment_list.php
@@ -0,0 +1,65 @@
+<?php
+include '../components/header.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>coliving</title>
+
+  <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+  <div class="container">
+    <table class="table">
+        <tr class="table_menu">
+          <th scope="col">apartment_id</th>
+          <th scope="col">Price(€)</th>
+
+          <th scope="col">Wifi</th>
+          <th scope="col">Beds</th>
+          <th scope="col">Rooms</th>
+          <th scope="col">Baths</th>
+          <th scope="col">Size</th>
+        </tr>
+        
+        <?php
+        include '../components/connect.php';
+        $sql = "Select * from `apartment` where available=true";
+        $result = mysqli_query($conn, $sql);
+        if ($result) {
+          while ($row = mysqli_fetch_assoc($result)) {
+            $id = $row['apartment_id'];
+            $price = $row['price'];
+
+            $wifi = $row['wifi'];
+            $beds = $row['beds'];
+            $rooms = $row['rooms'];
+            $baths = $row['baths'];
+            $size = $row['size'];
+            echo '<tr>
+    <td scope="row">' . $id . '</td>
+    <td>' . $price . '</td>
+   
+    <td>' . $wifi . '</td>
+    <td>' . $beds . '</td>
+    <td>' . $rooms . '</td>
+    <td>' . $baths . '</td>
+    <td>' . $size . '</td>
+  </tr>';
+          }
+        }
+
+        ?>
+    </table>
+      </div>
+</body>
+
+<?php
+include '../components/footer.php'
+?>
